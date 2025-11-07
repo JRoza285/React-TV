910cbea7ecb5ab6b1726c1ccfe35af8bfec23009
JRoza285 <jroza79@gmail.com>
2025-11-06 19:12:41 -0500

    modified jsx files

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,20 @@
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  if (!selectedShow) {
+  return (
+  <div className="show-details">
+  <h2>Show Details</h2>
+  <p>Select a show to learn more!</p>
+  </div>
+  );
+}
+return (
+  <div className="show-details">
+    <h2>Episode {show.number}</h2>
+    <h3>{show.title}</h3>
+    <p>{show.description}</p>
+  </div>
+);
 }
